9771ba0059f5a5907e71103f4666f984df8d330a
Lucian Buzzo <lucian@gravitywell.co.uk>
2016-05-20 10:45:52 +0100

    Updated Biscuit to user prototype for prettiness!

--- biscuit.js
+++ biscuit.js
@@ -1,24 +1,30 @@
-;(function(){
-function cookiesToObj(){ 
+!function() {
+  function cookiesToObj() {
     var obj = {};
     document.cookie.split('; ').forEach(function(v) {
-        var x = v.split('=');
-        obj[decodeURIComponent(x[0])] = decodeURIComponent(x[1]);
+      var x = v.split('=');
+      obj[decodeURIComponent(x[0])] = decodeURIComponent(x[1]);
     });
     return obj;
-}
-this.biscuit = {
-    get: function(key){
-        var cookies = cookiesToObj();
-        return (cookies.hasOwnProperty(key)?cookies[key]:false);
-    },
-    set: function(key, val, forever) {
-        document.cookie = encodeURIComponent(key) + '=' + encodeURIComponent(val)
-        + '; domain=' + window.location.host + '; path=/'
-        + (forever?'; expires=Fri, 31 Dec 9999 23:59:59 GMT':'');
-    },
-    remove: function(key) {
-        document.cookie = encodeURIComponent(key) + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; domain=' + window.location.host + '; path=/';
-    }
-}       
-}.call(this));
+  }
+
+  function Biscuit() {
+  }
+
+  Biscuit.prototype.get = function(key) {
+    var cookies = cookiesToObj();
+    return cookies.hasOwnProperty(key) ? cookies[key] : false;
+  };
+
+  Biscuit.prototype.set = function(key, val, forever) {
+    document.cookie = encodeURIComponent(key) + '=' + encodeURIComponent(val)
+    + '; domain=' + window.location.host + '; path=/'
+    + (forever ? '; expires=Fri, 31 Dec 9999 23:59:59 GMT' : '');
+  };
+
+  Biscuit.prototype.remove = function(key) {
+    document.cookie = encodeURIComponent(key) + '=; expires=Thu, 01 Jan 1970 00:00:00 GMT; domain=' + window.location.host + '; path=/';
+  };
+
+  this.biscuit = new Biscuit();
+}.call(this);
